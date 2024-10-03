d038902273e9ff374271ceb38870cdaa4f2676eb
novalrnv <akuunoval@gmail.com>
2024-10-03 08:05:15 +0700

    Soal 1

--- Pemilihan2Percobaan112.java
+++ Pemilihan2Percobaan112.java
@@ -9,8 +9,11 @@ public class Pemilihan2Percobaan112{
         tahun = input12.nextInt();
 
         if ((tahun % 4) == 0){
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) != 0){
                 System.out.println("Tahun Kabisat");
+            } else {
+            System.out.println("Bukan Tahun Kabisat");
+            }
         } else {
             System.out.println("Bukan Tahun Kabisat");
         }
