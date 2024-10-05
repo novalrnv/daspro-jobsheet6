aed57979bf4759174a456746e389ea17adefd77e
novalrnv <akuunoval@gmail.com>
2024-10-05 09:09:02 +0700

    Tugas Latihan

--- Tugas/latian3.java
+++ Tugas/latian3.java
@@ -15,49 +15,73 @@ public class latian3 {
 
         if(merk.equalsIgnoreCase("specs")){
             if(kategori.equalsIgnoreCase("slip on")) {
-                if(ukuran >= 36)
-                    if(ukuran <= 40)
+                if(ukuran >= 36){
+                    if(ukuran <= 40){
                         System.out.println("800.000");
-                else
+                    }else{
+                        System.out.println("Size tidak tersedia!");
+                    }
+                }else{
                     System.out.println("Size tidak tersedia!");
+                }
             }else if(kategori.equalsIgnoreCase("high top")) {
-                if(ukuran >= 40)
-                    if(ukuran <= 44)
+                if(ukuran >= 40){
+                    if(ukuran <= 44){
                         System.out.println("1.200.000");
-                else
+                    }else{
+                        System.out.println("Size tidak tersedia!");
+                    }
+                }else{
                     System.out.println("Size tidak tersedia!");
+                }
             }else{
                 System.out.println("Kategori tidak tersedia!");
             }
         }else if(merk.equalsIgnoreCase("910")){
             if(kategori.equalsIgnoreCase("woman")) {
-                if(ukuran >= 36)
-                    if(ukuran <= 41)
+                if(ukuran >= 36){
+                    if(ukuran <= 41){
                         System.out.println("1.000.000");
-                else
+                    }else{
+                        System.out.println("Size tidak tersedia!");
+                    }
+                }else{
                     System.out.println("Size tidak tersedia!");
+                }
             }else if(kategori.equalsIgnoreCase("man")) {
-                if(ukuran >= 41)
-                    if(ukuran <= 44)
+                if(ukuran >= 41){
+                    if(ukuran <= 44){
                         System.out.println("1.800.000");
-                else
+                    }else{
+                        System.out.println("Size tidak tersedia!");
+                    }
+                }else{
                     System.out.println("Size tidak tersedia!");
+                }
             }else{
                 System.out.println("Kategori tidak tersedia!");
             }
         }else if(merk.equalsIgnoreCase("ortus")){
             if(kategori.equalsIgnoreCase("kids")) {
-                if(ukuran >= 36)
-                    if(ukuran <= 40)
+                if(ukuran >= 36){
+                    if(ukuran <= 40){
                         System.out.println("250.000");
-                else
+                    }else{
+                        System.out.println("Size tidak tersedia!");
+                    }
+                }else{
                     System.out.println("Size tidak tersedia!");
+                }
             }else if(kategori.equalsIgnoreCase("adults")) {
-                if(ukuran >= 40)
-                    if(ukuran <= 44)
+                if(ukuran >= 40){
+                    if(ukuran <= 44){
                         System.out.println("1.500.000");
-                else
+                    }else{
+                        System.out.println("Size tidak tersedia!");
+                    }
+                }else{
                     System.out.println("Size tidak tersedia!");
+                }
             }else{
                 System.out.println("Kategori tidak tersedia!");
             }
